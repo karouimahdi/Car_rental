6d571b9370eb03df57dd84678a7da7248ba985e2
karouimahdi <98275441+karouimahdi@users.noreply.github.com>
2022-11-17 22:39:16 +0100

    connecting dataBase with java project

--- car-rental/src/main/java/com/epam/carrental/App.java
+++ car-rental/src/main/java/com/epam/carrental/App.java
@@ -1,17 +1,43 @@
 package com.epam.carrental;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import com.epam.carrental.domain.User;
 import com.epam.carrental.domain.UserRole;
+import com.epam.carrental.model.dao.impl.UserDao;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        User user = new User(1, "mahdi", "kar@gmail.com", "Ar", "fsdf",false, UserRole.ADMIN);
-        System.out.println("user:"+ user);  
-    }
+public class App {
+
+	public static void main(final String[] args) throws SQLException {
+
+		UserDao userDao = new UserDao();
+
+		System.out.println("============================ SELECT ALL ===============================");
+
+		List<User> users = userDao.getAllItems();
+		//users.forEach(System.out.println());
+
+		System.out.println("============================ SELECT BY ID ===============================");
+
+		User user = userDao.getItemById(4);
+		System.out.println(" Item By Id: " + user);
+		System.out.println("============================ DELETE ===============================");
+
+		System.out.println("Deleted Status: " + userDao.deleteItem(104));
+		List<User> usersAfterDelete = userDao.getAllItems();
+		//usersAfterDelete.forEach(System.out::println);
+		System.out.println("============================ UPDATE ===============================");
+		User userBeforeUpdate = userDao.getItemById(2);
+		System.out.println(" userBeforeUpdate: " + userBeforeUpdate);
+		User updateUser = new User(2, "userName", "userEmail", "userPassword", "userAddress", false, UserRole.ADMIN);
+		System.out.println("Updated Status: " + userDao.updateItem(updateUser));
+		User userAfterUpdate = userDao.getItemById(2);
+		System.out.println(" userAfterUpdate: " + userAfterUpdate);
+		System.out.println("============================ INSERT  ===============================");
+		User newuser = new User("userName", "New UserEmail", "userPassword", "userAddress", false, UserRole.ADMIN);
+		System.out.println("Insert Starus: " + userDao.insertItem(newuser));
+		List<User> userNew = userDao.getAllItems();
+		//userNew.forEach(System.out::println);
+	}
 }
